7bc360ee2dd7639d144c97b52707f3dec484cdd0
elvin12802 <86964173+elvin12802@users.noreply.github.com>
2021-07-11 10:11:36 +0400

    Update mjapp.py

--- mjapp.py
+++ mjapp.py
@@ -3,7 +3,7 @@ pip install adafruit-io --quiet
 API_KEY = os.getenv('API_KEY')
 from Adafruit_IO import Client
 aio = Client('ElvinE', 'API_KEY')
-!pip install python-telegram-bot==13.0 --quiet
+pip install python-telegram-bot==13.0 --quiet
 from telegram.ext import Updater, MessageHandler,Filters
 
 def lighton(bot,update):
